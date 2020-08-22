82d2332eca0d06ea968560afb45aad960fd9c879
peluza <edisonisaza@gmail.com>
2020-08-22 11:49:36 -0500

    2-sort.rb

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,20 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    result << i_arg
+end
+result_2 = result.sort
+
+puts result_2
